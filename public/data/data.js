17aaf4660d5d70c5ec51e18afa5373191928a3f4
TylerAllison7 <112003780+TylerAllison7@users.noreply.github.com>
2025-01-07 01:32:36 -0500

    Add files via upload

---
+++ public/data/data.js
@@ -0,0 +1,28 @@
+let meetings  = [
+    {
+        id: 0,
+        topic: "CIT Monthly Meeting", 
+        mandatory : true, 
+        dateTime: "September 19th 2024, 2pm-3pm", 
+        location: "Knoy Hall West Lafayette",
+        parking: "Park in the West Street Garage, 3rd floor. Venue opposite front entrance."
+    },
+    {
+        id: 1,
+        topic: "Research In Higher Level Ed", 
+        mandatory : false, 
+        dateTime: "September 24th 2024, 1pm-5pm", 
+        location: "Beresford Building, Room 2, West Lafayette",
+        parking: "Park in the surface lot 300. Venue beside lot."
+    },
+    {
+        id: 2,
+        topic: "Curriculum Planning", 
+        mandatory : true, 
+        dateTime: "October 19th 2024, 4pm-6pm", 
+        location: "IO240, Indianapolis",
+        parking: "Park in the North Street Garage, Michigan St. Venue opposite side of street, 300km North."
+    }
+];
+
+export default meetings;
